GerritCodeReview/plugins_hooks-its

Support to enable/disable the issue tracker integration per project
It can now be configured per project whether the issue tracker
integration is enabled or not. To enable the issue tracker integration
for a project the project must have the following entry in its
`project.config` file in the `refs/meta/config` branch:

  [plugin "<its-name>"]
    enabled = true

If `plugin.<its-name>.enabled` is not specified in the `project.config`
file the value is inherited from the parent project. If it is also not
set on any parent project the issue integration is disabled for this
project.

By setting `plugin.<its-name>.enabled` to true in the `project.config`
of the `All-Projects` project the issue tracker integration can be
enabled by default for all projects. During the initialization of the
plugin you are asked if the issue integration should be enabled by
default for all projects and if yes this setting in the
`project.config` of the `All-Projects` project is done automatically.

With this it is now possible to support integration with multiple
issue trackers on a server. E.g. a project can choose if it wants to
enable integration with Jira or with Bugzilla.

This uses new functionality in Gerrit and hence the Gerrit API
version needs to be changed to 2.9-SNAPSHOT.

Change-Id: I23edfb28006b8a34de985d13a2240dda022c4683
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

hooks-its/src/main/java/com/googlesource/gerrit/plugins/hooks/its/ItsConfig.java
@@ -1,0 +1,73 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.hooks.its;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.events.ChangeAbandonedEvent;
+import com.google.gerrit.server.events.ChangeEvent;
+import com.google.gerrit.server.events.ChangeMergedEvent;
+import com.google.gerrit.server.events.ChangeRestoredEvent;
+import com.google.gerrit.server.events.CommentAddedEvent;
+import com.google.gerrit.server.events.PatchSetCreatedEvent;
+import com.google.gerrit.server.events.RefUpdatedEvent;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItsConfig {
+  private static final Logger log = LoggerFactory.getLogger(ItsConfig.class);
+
+  private final String itsName;
+  private final PluginConfigFactory pluginCfgFactory;
+
+  @Inject
+  ItsConfig(@ItsName String itsName, PluginConfigFactory pluginCfgFactory) {
+    this.itsName = itsName;
+    this.pluginCfgFactory = pluginCfgFactory;
+  }
+
+  public boolean isEnabled(ChangeEvent event) {
+    if (event instanceof PatchSetCreatedEvent) {
+      return isEnabled(((PatchSetCreatedEvent) event).change.project);
+    } else if (event instanceof CommentAddedEvent) {
+      return isEnabled(((CommentAddedEvent) event).change.project);
+    } else if (event instanceof ChangeMergedEvent) {
+      return isEnabled(((ChangeMergedEvent) event).change.project);
+    } else if (event instanceof ChangeAbandonedEvent) {
+      return isEnabled(((ChangeAbandonedEvent) event).change.project);
+    } else if (event instanceof ChangeRestoredEvent) {
+      return isEnabled(((ChangeRestoredEvent) event).change.project);
+    } else if (event instanceof RefUpdatedEvent) {
+      return isEnabled(((RefUpdatedEvent) event).refUpdate.project);
+    } else {
+      log.debug("Event " + event + " not recognised and ignored");
+      return false;
+    }
+  }
+
+  public boolean isEnabled(String project) {
+    try {
+      return pluginCfgFactory.getFromProjectConfig(
+          new Project.NameKey(project), itsName).getBoolean("enabled", false);
+    } catch (NoSuchProjectException e) {
+      log.error("Failed to check if " + itsName + " is enabled for project "
+          + project, e);
+      return false;
+    }
+  }
+}

GerritCodeReview/plugins_its-base

its-base/src/main/java/com/googlesource/gerrit/plugins/hooks/its/ItsConfig.java
@@ -1,0 +1,74 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.hooks.its;
+
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.events.ChangeAbandonedEvent;
+import com.google.gerrit.server.events.ChangeEvent;
+import com.google.gerrit.server.events.ChangeMergedEvent;
+import com.google.gerrit.server.events.ChangeRestoredEvent;
+import com.google.gerrit.server.events.CommentAddedEvent;
+import com.google.gerrit.server.events.PatchSetCreatedEvent;
+import com.google.gerrit.server.events.RefUpdatedEvent;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItsConfig {
+  private static final Logger log = LoggerFactory.getLogger(ItsConfig.class);
+
+  private final String pluginName;
+  private final PluginConfigFactory pluginCfgFactory;
+
+  @Inject
+  public ItsConfig(@PluginName String pluginName, PluginConfigFactory pluginCfgFactory) {
+    this.pluginName = pluginName;
+    this.pluginCfgFactory = pluginCfgFactory;
+  }
+
+  public boolean isEnabled(ChangeEvent event) {
+    if (event instanceof PatchSetCreatedEvent) {
+      return isEnabled(((PatchSetCreatedEvent) event).change.project);
+    } else if (event instanceof CommentAddedEvent) {
+      return isEnabled(((CommentAddedEvent) event).change.project);
+    } else if (event instanceof ChangeMergedEvent) {
+      return isEnabled(((ChangeMergedEvent) event).change.project);
+    } else if (event instanceof ChangeAbandonedEvent) {
+      return isEnabled(((ChangeAbandonedEvent) event).change.project);
+    } else if (event instanceof ChangeRestoredEvent) {
+      return isEnabled(((ChangeRestoredEvent) event).change.project);
+    } else if (event instanceof RefUpdatedEvent) {
+      return isEnabled(((RefUpdatedEvent) event).refUpdate.project);
+    } else {
+      log.debug("Event " + event + " not recognised and ignored");
+      return false;
+    }
+  }
+
+  public boolean isEnabled(String project) {
+    try {
+      return pluginCfgFactory.getFromProjectConfigWithInheritance(
+          new Project.NameKey(project), pluginName).getBoolean("enabled", false);
+    } catch (NoSuchProjectException e) {
+      log.error("Failed to check if " + pluginName + " is enabled for project "
+          + project, e);
+      return false;
+    }
+  }
+}