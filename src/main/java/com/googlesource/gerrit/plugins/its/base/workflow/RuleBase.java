3bb38b89bd67734fba0884e866f064c704b1fb2f
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2018-02-10 07:39:37 -0500

    RuleBase: Read rules also from project config
    So far, the rules defining the conditions and the actions affecting an
    ITS issue were defined globally in <gerrit_site>/etc/its folder and all
    the projects were subject to the same rules.
    
    This can become a limitation as the handling of ITS issues can be very
    different from one project to another, even if they use the same ITS
    system.
    
    Allow rules to be defined at the project level. As with the current
    global rules, two files can be defined: 'actions.config' to define
    general rules at the project level and 'actions-<its-name>.config' to
    define rules specific to an ITS system. These files should be created in
    the refs/meta/config branch and, similar with what happens now with the
    global rules, the set of rules to be applied to an ITS issue is the sum
    of the rules defined in the two files.
    
    Once rules are defined at the project level, the global rules no longer
    have effect in the project, i.e., the more specific rules take
    precedence.
    
    As with other project defined configurations, inheritance is honored so
    rules can be defined in a parent project. This inheritance, however, is
    capped at the closest level, i.e., if a project defines at least one the
    rules files, the presence of these files is not evaluated for any of the
    project's parents.
    
    Change-Id: I2bb00a6c2687d41c9f4d8f73a0c0387ab3cb8834

--- src/main/java/com/googlesource/gerrit/plugins/its/base/workflow/RuleBase.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/base/workflow/RuleBase.java
@@ -14,15 +14,22 @@
 
 package com.googlesource.gerrit.plugins.its.base.workflow;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.config.SitePath;
+import com.google.gerrit.server.git.ProjectLevelConfig;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.eclipse.jgit.util.FS;
 import org.slf4j.Logger;
@@ -36,16 +43,18 @@ public class RuleBase {
   private static final String CONFIG_FILE_NAME = "actions%s.config";
 
   /** The section for rules within rulebases */
-  private static final String RULE_SECTION = "rule";
+  static final String RULE_SECTION = "rule";
 
   /** The key for actions within rulebases */
-  private static final String ACTION_KEY = "action";
+  static final String ACTION_KEY = "action";
 
   private final Path itsPath;
   private final Rule.Factory ruleFactory;
   private final Condition.Factory conditionFactory;
   private final ActionRequest.Factory actionRequestFactory;
-  private final String pluginName;
+  private final String globalRulesFileName;
+  private final String pluginRulesFileName;
+  private final ProjectCache projectCache;
 
   private Collection<Rule> rules;
 
@@ -59,45 +68,55 @@ public class RuleBase {
       Rule.Factory ruleFactory,
       Condition.Factory conditionFactory,
       ActionRequest.Factory actionRequestFactory,
+      ProjectCache projectCache,
       @PluginName String pluginName) {
     this.itsPath = sitePath.normalize().resolve("etc").resolve("its");
     this.ruleFactory = ruleFactory;
     this.conditionFactory = conditionFactory;
     this.actionRequestFactory = actionRequestFactory;
-    this.pluginName = pluginName;
+    this.projectCache = projectCache;
+    this.globalRulesFileName = String.format(CONFIG_FILE_NAME, "");
+    this.pluginRulesFileName = String.format(CONFIG_FILE_NAME, "-" + pluginName);
     reloadRules();
   }
 
   /**
-   * Adds rules from a file to the RuleBase.
+   * Gets rules from a file.
    *
    * <p>If the given file does not exist, it is silently ignored
    *
    * @param ruleFile File from which to read the rules
+   * @return A collection of rules or an empty collection if the file does not exist or contains an
+   *     invalid configuration
    */
-  private void addRulesFromFile(File ruleFile) {
+  private Collection<Rule> getRulesFromFile(File ruleFile) {
     if (ruleFile.exists()) {
       FileBasedConfig cfg = new FileBasedConfig(ruleFile, FS.DETECTED);
       try {
         cfg.load();
+        return getRulesFromConfig(cfg);
       } catch (IOException | ConfigInvalidException e) {
         log.error("Invalid ITS action configuration", e);
-        return;
-      }
-
-      for (String subsection : cfg.getSubsections(RULE_SECTION)) {
-        Rule rule = ruleFactory.create(subsection);
-        for (String key : cfg.getNames(RULE_SECTION, subsection)) {
-          String[] values = cfg.getStringList(RULE_SECTION, subsection, key);
-          if (ACTION_KEY.equals(key)) {
-            addActions(rule, values);
-          } else {
-            addConditions(rule, key, values);
-          }
-        }
-        rules.add(rule);
       }
     }
+    return Collections.emptyList();
+  }
+
+  private Collection<Rule> getRulesFromConfig(Config cfg) {
+    Collection<Rule> rules = new ArrayList<>();
+    for (String subsection : cfg.getSubsections(RULE_SECTION)) {
+      Rule rule = ruleFactory.create(subsection);
+      for (String key : cfg.getNames(RULE_SECTION, subsection)) {
+        String[] values = cfg.getStringList(RULE_SECTION, subsection, key);
+        if (ACTION_KEY.equals(key)) {
+          addActions(rule, values);
+        } else {
+          addConditions(rule, key, values);
+        }
+      }
+      rules.add(rule);
+    }
+    return rules;
   }
 
   private void addActions(Rule rule, String[] values) {
@@ -114,16 +133,15 @@ public class RuleBase {
 
   /** Loads the rules for the RuleBase. */
   private void reloadRules() {
-    rules = Lists.newArrayList();
+    rules = new ArrayList<>();
 
     // Add global rules
-    File globalRuleFile = itsPath.resolve(String.format(CONFIG_FILE_NAME, "")).toFile();
-    addRulesFromFile(globalRuleFile);
+    File globalRuleFile = itsPath.resolve(globalRulesFileName).toFile();
+    rules.addAll(getRulesFromFile(globalRuleFile));
 
     // Add its-specific rules
-    File itsSpecificRuleFile =
-        itsPath.resolve(String.format(CONFIG_FILE_NAME, "-" + pluginName)).toFile();
-    addRulesFromFile(itsSpecificRuleFile);
+    File itsSpecificRuleFile = itsPath.resolve(pluginRulesFileName).toFile();
+    rules.addAll(getRulesFromFile(itsSpecificRuleFile));
 
     if (!globalRuleFile.exists() && !itsSpecificRuleFile.exists()) {
       log.warn(
@@ -139,11 +157,57 @@ public class RuleBase {
    * @param properties The properties to search actions for.
    * @return Requests for the actions that should be fired.
    */
-  public Collection<ActionRequest> actionRequestsFor(Iterable<Property> properties) {
-    Collection<ActionRequest> ret = Lists.newLinkedList();
-    for (Rule rule : rules) {
-      ret.addAll(rule.actionRequestsFor(properties));
+  public Collection<ActionRequest> actionRequestsFor(Collection<Property> properties) {
+    String projectName = getProjectFromProperties(properties);
+    Collection<Rule> fromProjectConfig = getRulesForProject(projectName);
+    Collection<Rule> rulesToAdd = !fromProjectConfig.isEmpty() ? fromProjectConfig : rules;
+
+    Collection<ActionRequest> actions = new ArrayList<>();
+    for (Rule rule : rulesToAdd) {
+      actions.addAll(rule.actionRequestsFor(properties));
     }
-    return ret;
+    return actions;
+  }
+
+  private String getProjectFromProperties(Collection<Property> properties) {
+    for (Property property : properties) {
+      if ("project".equals(property.getKey())) {
+        return property.getValue();
+      }
+    }
+    return "";
+  }
+
+  private Collection<Rule> getRulesForProject(String projectName) {
+    if (projectName.isEmpty()) {
+      return Collections.emptyList();
+    }
+    Collection<Rule> projectRules = new ArrayList<>();
+    try {
+      ProjectState project = projectCache.checkedGet(new Project.NameKey(projectName));
+      projectRules = readRulesFrom(project);
+      if (projectRules.isEmpty()) {
+        for (ProjectState parent : project.parents()) {
+          projectRules = readRulesFrom(parent);
+          if (!projectRules.isEmpty()) {
+            break;
+          }
+        }
+      }
+    } catch (IOException e) {
+      log.debug("Ignoring project specific rules: unable to open project {}.", projectName, e);
+    }
+    return projectRules;
+  }
+
+  private Collection<Rule> readRulesFrom(ProjectState project) {
+    ProjectLevelConfig global = project.getConfig(globalRulesFileName);
+    Config general = global.get();
+    ProjectLevelConfig plugin = project.getConfig(pluginRulesFileName);
+    Config pluginSpecific = plugin.get();
+    return new ImmutableList.Builder<Rule>()
+        .addAll(getRulesFromConfig(general))
+        .addAll(getRulesFromConfig(pluginSpecific))
+        .build();
   }
 }
