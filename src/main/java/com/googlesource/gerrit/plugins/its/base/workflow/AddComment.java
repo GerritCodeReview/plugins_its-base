8e8a9cd48b708304f03ae7949eaa39267b8e2b79
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2018-03-05 13:28:02 -0500

    Restrict access to "internal" API
    There are some interfaces, like Action, that are used only by this
    plugin and not by the its-* child plugins. Their access could not be
    restricted as they are used by other classes in a different package.
    
    Remove the 'action' package and move the classes inside it to the
    'workflow' package so that interface can be defined package-private. In
    the same idea, limit the access to the methods in some classes in the
    'util' package.
    
    This change is needed work in order to simplify the gathering and
    storage of events properties, which is done in a follow-up commit.
    
    Change-Id: I198985da4ecda582846729d7f441e5b835aadf85

--- src/main/java/com/googlesource/gerrit/plugins/its/base/workflow/AddComment.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/base/workflow/AddComment.java
@@ -12,13 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.its.base.workflow.action;
+package com.googlesource.gerrit.plugins.its.base.workflow;
 
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.its.base.its.ItsFacade;
-import com.googlesource.gerrit.plugins.its.base.workflow.ActionRequest;
-import com.googlesource.gerrit.plugins.its.base.workflow.Property;
 import java.io.IOException;
 import java.util.Set;
 
