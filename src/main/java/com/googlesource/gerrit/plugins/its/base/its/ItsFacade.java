98be05df6d0b71d0189cdec8a984fe758ae28d07
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-05-30 20:46:38 +0200

    Ability to add an event property value to an ITS field
    Vet-Version: 3.14
    Change-Id: Ief770151ad38fb2f61f1ca93c0730c80f9814da8

--- src/main/java/com/googlesource/gerrit/plugins/its/base/its/ItsFacade.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/base/its/ItsFacade.java
@@ -31,6 +31,11 @@ public interface ItsFacade {
 
   public void addComment(String issueId, String comment) throws IOException;
 
+  default void addValueToField(String issueId, String value, String fieldId) throws IOException {
+    throw new UnsupportedOperationException(
+        "add-value-to-field is not currently implemented by " + getClass());
+  }
+
   public void performAction(String issueId, String actionName) throws IOException;
 
   public boolean exists(final String issueId) throws IOException;
