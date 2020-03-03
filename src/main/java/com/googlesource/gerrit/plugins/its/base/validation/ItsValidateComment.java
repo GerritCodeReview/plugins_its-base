0901bfc062ad99ae02615a730cb932f2a3ac6376
Antonio Barone <syntonyze@gmail.com>
2020-03-03 15:57:23 -0800

    Do not reject commit upon connectivity problems
    When the issue existence could not be checked against the issue tracker
    because of a connectivity problem, the push will be accepted and the
    reason of the failure logged and displayed to the user.
    
    Bug: Issue 12372
    Change-Id: Ib5cfa2fafa81f3adf6d50aed44e2f523c86fea5b

--- src/main/java/com/googlesource/gerrit/plugins/its/base/validation/ItsValidateComment.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/base/validation/ItsValidateComment.java
@@ -47,6 +47,12 @@ public class ItsValidateComment implements CommitValidationListener {
 
   @Inject private IssueExtractor issueExtractor;
 
+  private enum ItsExistenceCheck {
+    EXISTS,
+    DOESNT_EXIST,
+    CONNECTIVITY_FAILURE
+  }
+
   private List<CommitValidationMessage> validCommit(Project.NameKey project, RevCommit commit)
       throws CommitValidationException {
     List<CommitValidationMessage> ret = Lists.newArrayList();
@@ -63,16 +69,23 @@ public class ItsValidateComment implements CommitValidationListener {
           List<String> nonExistingIssueIds = Lists.newArrayList();
           client = itsFacadeFactory.getFacade(project);
           for (String issueId : issueIds) {
-            boolean exists = false;
+            ItsExistenceCheck existenceCheckResult;
             try {
-              exists = client.exists(issueId);
+              existenceCheckResult =
+                  client.exists(issueId)
+                      ? ItsExistenceCheck.EXISTS
+                      : ItsExistenceCheck.DOESNT_EXIST;
             } catch (IOException e) {
-              synopsis = "Failed to check whether or not issue " + issueId + " exists";
+              synopsis =
+                  "Failed to check whether or not issue "
+                      + issueId
+                      + " exists, due to connectivity issue. Commit will be accepted.";
               log.warn(synopsis, e);
               details = e.toString();
-              ret.add(commitValidationFailure(synopsis, details));
+              existenceCheckResult = ItsExistenceCheck.CONNECTIVITY_FAILURE;
+              ret.add(commitValidationFailure(synopsis, details, existenceCheckResult));
             }
-            if (!exists) {
+            if (existenceCheckResult == ItsExistenceCheck.DOESNT_EXIST) {
               nonExistingIssueIds.add(issueId);
             }
           }
@@ -95,7 +108,7 @@ public class ItsValidateComment implements CommitValidationListener {
             sb.append(" Issue-Tracker");
             details = sb.toString();
 
-            ret.add(commitValidationFailure(synopsis, details));
+            ret.add(commitValidationFailure(synopsis, details, ItsExistenceCheck.DOESNT_EXIST));
           }
         } else if (!itsConfig
             .getDummyIssuePattern()
@@ -118,7 +131,7 @@ public class ItsValidateComment implements CommitValidationListener {
           sb.append(" Issue-Tracker");
           details = sb.toString();
 
-          ret.add(commitValidationFailure(synopsis, details));
+          ret.add(commitValidationFailure(synopsis, details, ItsExistenceCheck.DOESNT_EXIST));
         }
         break;
       case OPTIONAL:
@@ -128,10 +141,12 @@ public class ItsValidateComment implements CommitValidationListener {
     return ret;
   }
 
-  private CommitValidationMessage commitValidationFailure(String synopsis, String details)
+  private CommitValidationMessage commitValidationFailure(
+      String synopsis, String details, ItsExistenceCheck existenceCheck)
       throws CommitValidationException {
     CommitValidationMessage ret = new CommitValidationMessage(synopsis + "\n" + details, false);
-    if (itsConfig.getItsAssociationPolicy() == ItsAssociationPolicy.MANDATORY) {
+    if (itsConfig.getItsAssociationPolicy() == ItsAssociationPolicy.MANDATORY
+        && existenceCheck != ItsExistenceCheck.CONNECTIVITY_FAILURE) {
       throw new CommitValidationException(synopsis, Collections.singletonList(ret));
     }
     return ret;
