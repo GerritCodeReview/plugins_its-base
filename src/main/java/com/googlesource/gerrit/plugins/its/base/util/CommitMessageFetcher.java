4332145c6f745318ca014e62b81958bd3c2ce618
Christian Aistleitner <christian@quelltextlich.at>
2020-06-20 00:31:53 +0200

    Extend CommitMessageFetcher to handle non-commit objects
    CommitMessageFetcher expected to fetch for commit objects and failed
    for non-commit objects.
    
    With the advent of NoteDB, RefUpdatedEvents sometimes contain
    non-commit objects. For example `refs/sequences/changes` typically
    references a blob. When this ref gets updated, Gerrit sends a
    corresponding RefUpdatedEvent with blobs as oldRev and newRev, which
    used to trip up CommitMessageFetcher.
    
    To avoid issues, we extend CommitMessageFetcher to work on ids of
    arbitrary objects.
    
    Change-Id: I892b9299f73e71660b1a7d77d7f282a4f563001e

--- src/main/java/com/googlesource/gerrit/plugins/its/base/util/CommitMessageFetcher.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/base/util/CommitMessageFetcher.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,22 +22,33 @@ public class CommitMessageFetcher {
     this.repoManager = repoManager;
   }
 
-  public String fetch(String projectName, String commitId) throws IOException {
+  public String fetch(String projectName, String objectId) throws IOException {
     try (Repository repo = repoManager.openRepository(Project.nameKey(projectName))) {
       try (RevWalk revWalk = new RevWalk(repo)) {
-        RevCommit commit = revWalk.parseCommit(ObjectId.fromString(commitId));
-        return commit.getFullMessage();
+        RevObject obj = revWalk.peel(revWalk.parseAny(ObjectId.fromString(objectId)));
+        if (obj instanceof RevCommit) {
+          return ((RevCommit) obj).getFullMessage();
+        }
+        // objectId was found, but it's not a commit.
+        // Since the objectId was found, it's nothing to worry about and we do not need to alert the
+        // user. We silently return the empty string as blobs, trees, ... do not have a proper
+        // commit message.
+        //
+        // Parsing a non-commit objectId (and reaching this point) will happen for example on NoteDB
+        // sites when Gerrit updates `refs/sequences/changes` (which does not point at a commit, but
+        // a blob) on All-Projects and the corresponding RevUpdatedEvent gets processed.
+        return "";
       }
     }
   }
 
-  public String fetchGuarded(String projectName, String commitId) {
+  public String fetchGuarded(String projectName, String objectId) {
     String ret = "";
     try {
-      ret = fetch(projectName, commitId);
+      ret = fetch(projectName, objectId);
     } catch (IOException e) {
       log.error(
-          "Could not fetch commit message for commit " + commitId + " of project " + projectName,
+          "Could not fetch commit message for commit " + objectId + " of project " + projectName,
           e);
     }
     return ret;
